cb4ba5b8d988df35814bfcadb73a63d43f8a7e31
Warner Nielsen <bwn888@gmail.com>
2019-04-24 16:12:36 -0600

    Add case statements, update hashing functions

--- src/nielwarnproj10/NielWarnProj10.java
+++ src/nielwarnproj10/NielWarnProj10.java
@@ -31,6 +31,12 @@ public class NielWarnProj10 {
                                 "switch", "synchronized", "this", "throw",
                                 "throws", "transient", "try", "void",
                                 "volatile", "while", "true", "false", "null"};
+        char[] validIdent = {'$', '_', 'a', 'b', 'c', 'd',
+                                'e', 'f', 'g', 'h', 'i', 'j',
+                                'k', 'l', 'm', 'n', 'o', 'p',
+                                'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z'};
+        
         String item;
         
         System.out.println("Welcome to the Symbol Table Checker. Enter a variable name to\n" +
@@ -38,14 +44,64 @@ public class NielWarnProj10 {
                             "keyword, I'll let you know.");
         
         int userMenuChoice;
-        String userKeyword;
+        String userKeyword, deletedKeyword;
         boolean again = true;
+        boolean valid;
         Scanner userInput = new Scanner(System.in); // user input
         
         // display menu choices to run each part of Table class
         do {
             System.out.println("\nSymbol Table Menu:");
+            System.out.println("\t1 - Insert an item");
+            System.out.println("\t2 - Remove an item");
+            System.out.println("\t3 - Display all items");
+            System.out.println("\t4 - Quit");
+            
+            System.out.print("\nEnter a choice: ");
+            userMenuChoice = userInput.nextInt();
+            
+            // switch statement for menu choices
+            switch (userMenuChoice) {
+                case 1:
+                    System.out.print("Please enter a string to enter into the symbol table: ");
+                    userKeyword = userInput.next();
+                    valid = validSymbol(symbolArray, userKeyword);
+                    if (valid) {
+                        System.out.println("Cannot use key word '" + userKeyword + "'");
+                    } else {
+                        symbolTable.insert(userKeyword);
+                        System.out.println("Key word '" + userKeyword + "' entered into symbol table");
+                    }
+                    break;
+                case 2:
+                    System.out.print("Please enter a string to remove from symbol table: ");
+                    userKeyword = userInput.next();
+                    deletedKeyword = symbolTable.delete(userKeyword);
+                    if (deletedKeyword != null) {
+                        System.out.println("'" + deletedKeyword + "' removed from table");
+                    } else {
+                        System.out.println("'" + userKeyword + "' is not in the table, not removed from table");
+                    }
+                    break;
+                case 3:
+                    symbolTable.viewItems();
+                    break;
+                case 4:
+                    again = false;
+                    break;
+                default:
+                        System.out.println("Invalid choice, please try again...");
+            }
         } while (again);
     }
     
+    public static boolean validSymbol(String[] symbolArr, String key) {
+        boolean valid = false;
+        for (String item : symbolArr) {
+            if (item.compareTo(key) == 0) {
+                valid = true;
+            }
+        }
+        return valid;
+    }
 }
--- src/nielwarnproj10/Table.java
+++ src/nielwarnproj10/Table.java
@@ -15,16 +15,16 @@
 package nielwarnproj10;
 
 public class Table<T extends Comparable<? super T>> {
-    final int MAX_SIZE = 23;
-    final int HASH_SIZE_2 = 19;
+    final int MAX_SIZE = 11;
+    final int HASH_SIZE_2 = 7;
     private int size; // keep track of items array size
-    private final boolean[] usedArray; // array to hold if index has been used
+//    private final boolean[] usedArray; // array to hold if index has been used
     private final String[] items; // array called items
     
     // default constructor
     public Table() {
         items = new String[MAX_SIZE];
-        usedArray = new boolean[MAX_SIZE];
+//        usedArray = new boolean[MAX_SIZE];
     }
     
     // method to insert an item into the table as long
@@ -49,7 +49,7 @@ public class Table<T extends Comparable<? super T>> {
         // assigns the new item to the proper index
         items[hash1] = newItem;
         // sets the usedArray to true for that index
-        usedArray[hash1] = true;
+//        usedArray[hash1] = true;
         size++; // increment size of array
     }
     
@@ -60,37 +60,46 @@ public class Table<T extends Comparable<? super T>> {
         int hash2 = hashTwo(key);
         String value = null;
         
-        // if array size is zero the print a message and get out of method
-        if (size == 0) {
-            System.out.println("Table is empty");
-            return "";
+        while (items[hash1] != null && !items[hash1].equals(key)) {
+            hash1 += hash2;
+            hash1 %= MAX_SIZE;
         }
+        value = items[hash1];
+        items[hash1] = null;
+        size--;
+        return value;
         
-        // if the item at hash1 is null then print that the
-        // item doesn't exist and return null
-        if (items[hash1] == null) {
-            System.out.println("Item does not exist");
-            return value;
-        // if the item at hash1 is equal to the key AND the usedArray at hash1
-        // is set to true
-        }else if (items[hash1].compareTo(key) == 0 && usedArray[hash1] == true) {
-            value = items[hash1]; // assign the item at hash1 to value
-            items[hash1] = null; // remove item at hash1
-            size--; // decrement the size of items in the array
-            return value; // return the value deleted
-        // otherwise, if the value at hash1 does not equal the key
-        } else if (items[hash1].compareTo(key) != 0) {
-            // set hash1 equal to hash1 + hash2 mod the array size
-            hash1 = (hash1 + hash2) % MAX_SIZE; 
-            value = items[hash1]; // set value equal to the item at the new hash1
-            items[hash1] = null; // remove the item at new hash1
-            size--; // decrement the size of items in array
-            return value; // return value deleted
-        // otherwise
-        } else {
-            System.out.println("Couldn't find key");
-            return value;
-        }
+//        // if array size is zero the print a message and get out of method
+//        if (size == 0) {
+//            System.out.println("Table is empty");
+//            return "";
+//        }
+//        
+//        // if the item at hash1 is null then print that the
+//        // item doesn't exist and return null
+//        if (items[hash1] == null) {
+//            System.out.println("Item does not exist");
+//            return value;
+//        // if the item at hash1 is equal to the key AND the usedArray at hash1
+//        // is set to true
+//        } else if (items[hash1].compareTo(key) == 0 && usedArray[hash1] == true && items[hash1] != null) {
+//            value = items[hash1]; // assign the item at hash1 to value
+//            items[hash1] = null; // remove item at hash1
+//            size--; // decrement the size of items in the array
+//            return value; // return the value deleted
+//        // otherwise, if the value at hash1 does not equal the key
+//        } else if (items[hash1].compareTo(key) != 0) {
+//            // set hash1 equal to hash1 + hash2 mod the array size
+//            hash1 = (hash1 + hash2) % MAX_SIZE; 
+//            value = items[hash1]; // set value equal to the item at the new hash1
+//            items[hash1] = null; // remove the item at new hash1
+//            size--; // decrement the size of items in array
+//            return value; // return value deleted
+//        // otherwise
+//        } else {
+//            System.out.println("Couldn't find key");
+//            return value;
+//        }
     }
     
     // method to view all the items in the array and print the index
@@ -110,10 +119,16 @@ public class Table<T extends Comparable<? super T>> {
     // hash function that obtains the original index location in the array
     private int hashOne(String key) {
         int hashVal = 0;
+        int letter = 0;
         for (int i = 0; i < key.length(); i++) {
             // for lowercase letters takes char at i and subtracts 96 to get
-            // it's place in the alphabet equivalent
-            int letter = key.charAt(i) - 96; 
+            // its place in the alphabet equivalent
+            if ((key.charAt(i) - 96) > 0) {
+                letter = key.charAt(i) - 96; 
+            // for upper case letters
+            } else if ((key.charAt(i) - 46 > 0)) {
+                letter = key.charAt(i) - 46; 
+            }
             // use Horner's rule 
             hashVal = (hashVal * 32 + letter) % MAX_SIZE; 
         }
@@ -124,10 +139,16 @@ public class Table<T extends Comparable<? super T>> {
     // first number returned by hash one to help avoid collisions
     private int hashTwo(String key) {
         int hashVal = 0;
+        int letter = 0;
         for (int i = 0; i < key.length(); i++) {
             // for lowercase letters takes char at i and subtracts 96 to get
-            // it's place in the alphabet equivalent
-            int letter = key.charAt(i) - 96; 
+            // its place in the alphabet equivalent
+            if ((key.charAt(i) - 96) > 0) {
+                letter = key.charAt(i) - 96; 
+            // for upper case letters
+            } else if ((key.charAt(i) - 46 > 0)) {
+                letter = key.charAt(i) - 46;
+            }
             // use Horner's rule 
             hashVal = HASH_SIZE_2 - (hashVal * 32 + letter) % HASH_SIZE_2; 
         }
